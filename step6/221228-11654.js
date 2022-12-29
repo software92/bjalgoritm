9a66ab61e43584ca253fd48a6162e5aa45290a8d
jsw <allmie@naver.com>
2022-12-29 22:52:11 +0900

    doc: README.md 업데이트

--- step6/221228-11654.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString();
-
-const result = input.charCodeAt();
-
-console.log(result);
-
-// String.charCodeAt(): 문자를 아스키코드로 변환하는 함수
--- step6/221228-11720.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().split('\n');
-
-const count = +input[0];
-const numArr = input[1].split('', count);
-const sum = numArr.reduce((acc, cur) => +acc + +cur);
-
-console.log(sum);
